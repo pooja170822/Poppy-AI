5ac2131b824579e9f9bb2c5ac32cf92b13d74587
Pooja <85984734+pooja170822@users.noreply.github.com>
2024-09-24 13:14:15 +0530

    Add files via upload

---
+++ src/assets/assets.js
@@ -0,0 +1,29 @@
+import plus_icon from './plus_icon.png'
+import question_icon from './question_icon.png'
+import setting_icon from './setting_icon.png'
+import user_icon from './user_icon.jpg'
+import message_icon from './message_icon.png'
+import send_icon from './send_icon.png'
+import arrow_icon from './arrow_icon.png'
+import side_bar from './side_bar.png'
+import document from './document.png'
+import camera from './camera.png'
+import mic_icon from './mic_icon.png'
+
+
+
+
+export const assets = {
+    
+    plus_icon,
+    question_icon,
+    setting_icon,
+    user_icon,
+    message_icon,
+    send_icon,
+    arrow_icon,
+    side_bar,
+    camera,
+    document,
+    mic_icon
+}
